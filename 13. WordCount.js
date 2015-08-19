8492d8d9b66ce897b5f7874e3cfaa832dd84b68b
krisalbert <krisalbert@gmail.com>
2015-08-19 11:14:21 -0700

    Challenge to return the word count of a given argument

---
+++ 13. WordCount.js
@@ -0,0 +1,13 @@
+function WordCount(str) {
+  
+  var myArray = str.split(" ");
+ 
+ 
+  // code goes here  
+  return myArray.length; 
+         
+}
+   
+// keep this function call here 
+// to see how to enter arguments in JavaScript scroll down
+WordCount(readline());    
