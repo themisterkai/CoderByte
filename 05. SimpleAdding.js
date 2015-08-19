7890194248ab3d316571068de4a9c7d40f04bfdc
krisalbert <krisalbert@gmail.com>
2015-08-18 21:40:59 -0700

    challenge to add up all the numbers from 1 to a certain argument

---
+++ 05. SimpleAdding.js
@@ -0,0 +1,15 @@
+function SimpleAdding(num) {
+  var myNum=0;
+  
+  for(x=0;x<=num;x++){
+    myNum+=x;
+  }
+
+  // code goes here  
+  return myNum; 
+         
+}
+   
+// keep this function call here 
+// to see how to enter arguments in JavaScript scroll down
+SimpleAdding(readline());    
