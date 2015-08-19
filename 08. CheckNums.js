a36560d21d69aca18e4a2c88ea9f702eda11b6e8
krisalbert <krisalbert@gmail.com>
2015-08-18 21:41:12 -0700

    challenge to compare 2 numbers

---
+++ 08. CheckNums.js
@@ -0,0 +1,18 @@
+function CheckNums(num1,num2) { 
+
+  // code goes here
+  if(num2>num1){
+    return true;
+  }
+  else if(num2<num1){
+    return false;
+  }
+  else {
+    return -1;
+  }
+
+}
+   
+// keep this function call here 
+// to see how to enter arguments in JavaScript scroll down
+CheckNums(readline());           
