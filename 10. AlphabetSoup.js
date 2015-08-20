c344d69e5529ba3a1fe1f8277f72dc28c9648902
krisalbert <krisalbert@gmail.com>
2015-08-19 21:44:45 -0700

    Challenge to sort characters in a string.

---
+++ 10. AlphabetSoup.js
@@ -0,0 +1,13 @@
+function AlphabetSoup(str) { 
+  
+  return str.split("").sort().join("");
+  
+
+  // code goes here  
+  //return str; 
+         
+}
+   
+// keep this function call here 
+// to see how to enter arguments in JavaScript scroll down
+AlphabetSoup(readline());   
