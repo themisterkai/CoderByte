79881e3dae7097740036f5229e1b2c0ff7fb0c38
krisalbert <krisalbert@gmail.com>
2015-08-21 14:06:59 -0700

    Challenge to divide two numbers and return them in a certain format. This was a redo.

--- 20. DivisionStringified.js
+++ 20. DivisionStringified.js
@@ -1,33 +1,20 @@
 function DivisionStringified(num1,num2) { 
   
-  var quotient = Math.ceil(num1/num2).toString();
-  var length = quotient.length;
-  //return length;
-  
-  if(length==7){
-    return quotient.substring(0,1)+","+quotient.substring(1,4)+","+quotient.substring(4,7);
-  }
-  else if(length==6){
-    return quotient.substring(0,3)+","+quotient.substring(3,6);
-  }
-  else if(length==5){
-    return quotient.substring(0,2)+","+quotient.substring(2,5);
-  }
-  else if(length==4){
-    return quotient.substring(0,1)+","+quotient.substring(1,4);
-  }
-  else{
-    return quotient;
-  }
+  var quotient = Math.round(num1/num2).toString();
 
-  // code goes here  
-  //return num1 + num2; 
-         
+  quotient = quotient.split("").reverse().join("");
+  var holder = [];
+  
+  for(x=0;x<quotient.length;x+=3){
+    for(i=0;i<3;i++){
+      holder.push(quotient[x+i]);
+    }
+    holder.push(",");
+  }
+  return holder.reverse().join("").substring(1)
 }
    
 // keep this function call here 
 // to see how to enter arguments in JavaScript scroll down
-DivisionStringified(readline());    
+DivisionStringified(readline());  
 
-//this code needs to be rewritten, it works but it only works with brute force
-//it only works for up to 7 character quotients
